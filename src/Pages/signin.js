a771a6414dd8880691726bc4a16cbf89ca929a91
AbhayRana14 <14abhayrana@gmail.com>
2021-02-07 18:02:46 +0530

    Sign In component styling and compliting the site

--- src/Pages/signin.js
+++ src/Pages/signin.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ScrollToTop from '../Components/ScrollToTop';
+import SignIn from '../Components/Signin';
 
 const SigninPage = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <ScrollToTop/>
+            <SignIn/>
+        </>
     )
 }
 
